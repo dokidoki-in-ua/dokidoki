5f61927cf98f3a906703648939d25cfc54a93e68
Mykyta Kasianenko <mykytakasianenko@gmail.com>
2024-02-03 17:40:54 +0200

    feat: mongo async

--- apps/dokidoki-api/src/databases/mongo/mongo.module.ts
+++ apps/dokidoki-api/src/databases/mongo/mongo.module.ts
@@ -1,7 +1,16 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-  imports: [MongooseModule.forRoot(process.env.MONGO_DATABASE_URL)],
+  imports: [
+    MongooseModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: async (config: ConfigService) => ({
+        uri: config.get('MONGO_DATABASE_URL'),
+      }),
+      inject: [ConfigService],
+    }),
+  ],
 })
 export class MongoModule {}
