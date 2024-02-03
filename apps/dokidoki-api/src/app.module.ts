628cc35e4bdb835bf4932130afd57f2454a050e6
Mykyta Kasianenko <mykytakasianenko@gmail.com>
2024-02-03 17:12:32 +0200

    feat: users

--- apps/dokidoki-api/src/app.module.ts
+++ apps/dokidoki-api/src/app.module.ts
@@ -3,9 +3,11 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { DatabasesModule } from './databases/databases.module';
 import { ConfigsModule } from './configs/configs.module';
+import { UsersModule } from './users/users.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [DatabasesModule, ConfigsModule],
+  imports: [DatabasesModule, ConfigsModule, UsersModule, AuthModule],
   controllers: [AppController],
   providers: [AppService],
 })
