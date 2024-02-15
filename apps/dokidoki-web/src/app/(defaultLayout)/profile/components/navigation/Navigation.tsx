121d908c8e4e8c96f66b3e2449b7082b47e7cdca
Kyrylo <dexter37814@gmail.com>
2024-02-15 09:40:26 +0200

    fix(styles): tab's hover effect

--- apps/dokidoki-web/src/app/(defaultLayout)/profile/components/navigation/Navigation.tsx
+++ apps/dokidoki-web/src/app/(defaultLayout)/profile/components/navigation/Navigation.tsx
@@ -12,7 +12,7 @@ const Navigation: React.FC<NavigationProps> = ({ searchParams }) => {
     const currentTab = Object.keys(searchParams)[0] || ''
 
     return (
-        <div className='mt-4 lg:ml-32 hidden sm:flex'>
+        <div className='mt-4 hidden sm:flex lg:ml-32'>
             <div className='flex items-center text-sm uppercase text-span md:text-base'>
                 <div className='avatar mr-1 h-6 w-6 overflow-hidden rounded-sm bg-primary'>
                     <Image src={avatar} alt='Avatar' />
@@ -20,17 +20,22 @@ const Navigation: React.FC<NavigationProps> = ({ searchParams }) => {
                 <div className='flex'>
                     {tabs.map((item, index) => (
                         <div
-                            className={`after:px-2 md:after:px-3 after:text-span after:content-["/"] last:after:px-0 last:after:content-[""] ${
+                            className={`after:px-2 after:text-span after:content-["/"] last:after:px-0 last:after:content-[""] md:after:px-3 ${
                                 currentTab ===
                                 (item.link === '/profile'
                                     ? ''
                                     : item.link.slice(1))
-                                    ? 'text-font-hover drop-shadow-[0_0_10px_rgba(255,255,255,1)]'
+                                    ? 'text-font-hover drop-shadow-[0_0_8px_rgba(255,255,255,1)]'
                                     : ''
                             }`}
                             key={index}
                         >
-                            <Link href={item.link}>{item.name}</Link>
+                            <Link
+                                href={item.link}
+                                className='[transition:color_.2s_ease] hover:text-font-hover '
+                            >
+                                {item.name}
+                            </Link>
                         </div>
                     ))}
                 </div>
